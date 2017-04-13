566930525b76d9af526c7c01e9b9f8ff61e117f9
hwangc <hwangc@hwangc.com>
2017-04-13 16:49:36 +0900

    naver folder structure updated

--- src/data/naver/test/APIClawling.java
+++ src/data/naver/test/APIClawling.java
@@ -1,46 +1,53 @@
-package data.fetch.naver.test;
+package data.naver.test;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-//import java.io.IOException;
 import java.io.InputStreamReader;
-//import java.io.UnsupportedEncodingException;
-//import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class APIClawling {
 
 	public static void main(String[] args) throws Exception {
-		// get the json data from kofic api
+		// get the json data from naver api
 
 		// Movie detail info
-//		String ClientID = "TKJl4cSjK1WjO5Z64dan";
-//		String ClientSecret = "w5fz9r5yf4";
-		// TODO Access Movie with Naver API  
-		String movieCd = "미녀";
-		String sMVDetailUrl = "https://openapi.naver.com/v1/search/movie.json?query=";
-		// Staff detail info
-		// String sMVStaffDetailUrl =
-		// "http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleInfo.json?key=430156241533f1d058c603178cc3ca0e&peopleCd=20164556";
-		String temp = null;
-		String sMVDetailTempUrl = null;
-		StringBuffer sbMVDetailUrl = new StringBuffer();
+		String ClientID = "TKJl4cSjK1WjO5Z64dan";
+		String ClientSecret = "w5fz9r5yf4";
+		
+		BufferedReader bfMVDetail = null;
+		String movieCd = URLEncoder.encode("미녀와 야수", "UTF-8");;
+		String sMVDetailUrl = "https://openapi.naver.com/v1/search/movie.json?query="+movieCd;
+		String inputLine = null;
+		StringBuffer response = new StringBuffer();
 		BufferedWriter out = new BufferedWriter(new FileWriter("test_naver.csv"));
-
-		for (int i = 1; i < 2; i++) {
-			sMVDetailTempUrl = sMVDetailUrl + movieCd;
-			URL uMVDetailUrl = new URL(sMVDetailTempUrl);
-			BufferedReader bfMVDetailUrl = new BufferedReader(
-					new InputStreamReader(uMVDetailUrl.openStream(), "UTF-8"));
-			temp = bfMVDetailUrl.readLine();
-			sbMVDetailUrl.append(temp);
-
-			out.write(temp);
+		
+		URL uMVDetailUrl = new URL(sMVDetailUrl);
+		HttpURLConnection httpNvrSVRCon = (HttpURLConnection)uMVDetailUrl.openConnection();
+        httpNvrSVRCon.setRequestMethod("GET");
+        httpNvrSVRCon.setRequestProperty("X-Naver-Client-Id", ClientID);
+        httpNvrSVRCon.setRequestProperty("X-Naver-Client-Secret", ClientSecret);
+        
+        int responseCode = httpNvrSVRCon.getResponseCode();		
+        
+        if(responseCode==200) { 
+		    // 정상 호출
+        	bfMVDetail = new BufferedReader(new InputStreamReader(httpNvrSVRCon.getInputStream()));
+        } else {  
+        	// 에러 발생
+        	bfMVDetail = new BufferedReader(new InputStreamReader(httpNvrSVRCon.getErrorStream()));
+        }
+        
+        while ((inputLine = bfMVDetail.readLine()) != null) {
+            response.append(inputLine);
+            out.write(response.toString());
 		}
+        
 		out.close();
-		System.out.println(sbMVDetailUrl);
-
+        bfMVDetail.close();
+        System.out.println(response.toString());
 	}
 
 }
